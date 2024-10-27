8830c946f193f42b2f407c722ed5c2bc539ad846
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-27 20:03:44 +0100

    Creazione modelli

--- weighings/models.py
+++ weighings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Weigh(models.Model):
+    weigh = models.DecimalField(max_digits = 10, decimal_places = 3)
+    created_on = models.DateTimeField(auto_now_add = True)
+
+
