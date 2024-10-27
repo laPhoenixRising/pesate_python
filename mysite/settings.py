0766d3edaeb8e7bcf0d3993d4d1ab967ee72181e
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-27 21:57:45 +0100

    Attivazione modelli

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'weighings.apps.WeighingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
