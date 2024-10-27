065e9ec6d7a998431c89d9aacf65c0c110947a52
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-27 23:35:19 +0100

    Aggiunta metrodo __str__ alla classe weigh

--- weighings/models.py
+++ weighings/models.py
@@ -4,4 +4,6 @@ class Weigh(models.Model):
     value = models.DecimalField(max_digits = 10, decimal_places = 3)
     created_on = models.DateTimeField(auto_now_add = True)
 
+    def __str__(self): 
+        return f"{self.value} {self.created_on}"
 
