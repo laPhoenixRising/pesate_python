1a88b213857870e123cb2e7f761ace7a43a213c8
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-29 23:14:00 +0100

    Aggiornamento views index

--- weighings/views.py
+++ weighings/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
+from .models import Weigh
+
 def index(request):
-    return HttpResponse("Qui si inseriranno le pesate")
+    latest_value_list = Weigh.objects.order_by("-created_on")[:5]
+    
+    output = ", ".join([f"{q.value} {q.created_on}" for q in latest_value_list])
+
+    return HttpResponse(output)
