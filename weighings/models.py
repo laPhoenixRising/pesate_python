6074853c377363349c5d4dfa50b50fb5326fde9a
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-29 23:56:55 +0100

    Modifica visualizzazione ora

--- weighings/models.py
+++ weighings/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Weigh(models.Model):
     value = models.DecimalField(max_digits = 10, decimal_places = 2)
-    created_on = models.DateTimeField(auto_now_add = True)
+    created_on = models.DateField(auto_now_add = True)
 
     def __str__(self): 
         return f"{self.value} {self.created_on}"
