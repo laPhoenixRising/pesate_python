a0ed4655bb215ffd17e41b7de9c9672614d10533
LaPhoenixRising <laphoenixrising88@gmail.com>
2024-10-29 23:28:49 +0100

    Inserimento ritorno a capo lista ultime pesate

--- weighings/views.py
+++ weighings/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 from .models import Weigh
 
 def index(request):
-    latest_value_list = Weigh.objects.order_by("-created_on")[:5]
-    
-    output = ", ".join([f"{q.value} {q.created_on}" for q in latest_value_list])
-
+    latest_weighs = Weigh.objects.order_by("-created_on")[:5]
+    list = [f"{weigh.value} {weigh.created_on}" for weigh in latest_weighs]
+    output = "<br>".join(list)
     return HttpResponse(output)
